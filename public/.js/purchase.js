76de00edd884ee00ebd04052645d2b9c350388f6
Victoria Chen <54255894+victoriaychen@users.noreply.github.com>
2021-04-15 21:00:15 -0700

    mysql connection

--- public/.js/purchase.js
+++ public/.js/purchase.js
@@ -2,10 +2,10 @@ function validateStock() {
     var ticker = document.forms['stock']['ticker'].value;
     var quantity = document.forms['stock']['qty'].value;
     quantity = parseInt(quantity);
-    if(quantity < 10 || quantity > 100){
+    if (quantity < 10 || quantity > 100) {
         alert("INVALID QUANTITY");
-    }else{
-        alert("Confirm " + ticker + " " + quantity + " ?" );
+    } else {
+        alert("Confirm " + ticker + " " + quantity + " ?");
         printData();
     }
 }
@@ -16,4 +16,22 @@ function printData() {
     quantity = parseInt(quantity);
 
     document.getElementById("purchase_data").innerHTML = "<table class='table table-dark'><tr><th>Ticker</th><th>Quantity</th></tr><tr><td>" + ticker + "</td><td>" + quantity + " </td></tr></table>"
+}
+
+function storeData() {
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "username",
+        password: "userpassword"
+        database: "mydb"
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+    });
+
+
 }
