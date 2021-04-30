1153d1c9aa19e906677b69180b7c4efed21fde2f
Victoria Chen <54255894+victoriaychen@users.noreply.github.com>
2021-04-30 14:59:27 -0700

    Display data

--- public/.js/purchase.js
+++ public/.js/purchase.js
@@ -10,6 +10,25 @@ function validateStock() {
     }
 }
 
+const getStock = async ticker=>{
+	console.log("Getting data");
+  	const request = await fetch('https://cors-anywhere.herokuapp.com/https://stocksapi.herokuapp.com/stock',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'ticker': ticker,
+            'type': 'daily'
+        })
+    })
+    const data = await request.json()
+    console.log(data);
+    return data;
+}
+
+//getStock('AAPL');
+
 // To Do: display data from mongodb, login page
 function printData() {
     console.log("inside printData");
@@ -19,7 +38,7 @@ function printData() {
     console.log(ticker, quantity);
 
     quantity = parseInt(quantity);
-    document.getElementById("purchase_data").innerHTML = "<table class='table table-dark'><tr><th>Ticker</th><th>Quantity</th></tr><tr><td>" + ticker + "</td><td>" + quantity + " </td></tr></table>"
+    // document.getElementById("purchase_data").innerHTML = "<table class='table table-light'><tr><th>Ticker</th><th>Quantity</th></tr><tr><td>" + ticker + "</td><td>" + quantity + " </td></tr></table>"
 
     txt_data = {"ticker": ticker, "quantity": quantity};
     console.log(txt_data);
@@ -38,6 +57,47 @@ function printData() {
       } else {
         window.location.href = "/purchase.html"
       }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+    fetchData();
+}
+
+function fetchData() {
+    fetch("/fetch_data", {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+    console.log('Success:', data);
+
+    num_entries = data.entry.length;
+
+    //CODE TO GENERATE ENTRIES
+    entries_report = "<table class='table table-light'><tr><th>Ticker</th><th>Quantity</th><th>Most recent price</th></tr>";
+
+    console.log(JSON.stringify(data.entry))
+    for (i = num_entries - 1; i >= 0; i--) {
+
+        console.log(data.entry[i])
+            // var dt = new Date(data.entries[i].ticker)
+            console.log("TRYING FOR "+i)
+
+            entries_report += "<tr><td>"
+                           + data.entry[i].ticker
+                           + "</td><td>"
+                           + data.entry[i].quantity
+                           + "</td><td>"
+                           + getStock(data.entry[i].ticker)
+                           + "</td></tr>";
+      }
+      entries_report += "</table>";
+
+      document.getElementById("purchase_data").innerHTML = entries_report;
     })
     .catch((error) => {
       console.error('Error:', error);
--- server.js
+++ server.js
@@ -11,9 +11,9 @@ var port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/example', async (req, res) => {
-  console.log("inside example.html ");
-});
+// app.get('/example', async (req, res) => {
+//   console.log("inside example.html ");
+// });
 
 app.post('/purchase', async (req, res) => {
     console.log("inside purchase.html ");
@@ -51,4 +51,95 @@ app.post('/purchase_onetime', async (req, res) => {
     } finally {
         client.close();
     }
+})
+
+app.get('/fetch_data', async (req, res) => {
+  const client = await MongoClient.connect(mongo_url)
+  try {
+    const dbo = client.db("tradefolio");
+    var coll = dbo.collection('stocks');
+
+    theQuery = { "_id" : ObjectId("608b86cd3c8839d801a1f51a")}
+
+    console.log("before items_found" );
+    var items_found = await coll.findOne(theQuery);
+    if (!items_found){
+      return res.status(400).json({message: "item not found"});
+    }
+    console.log("items_found " + items_found);
+
+    res.status(200).send(items_found);
+
+  } catch (err) {
+      console.log(err);
+  } finally {
+      client.close();
+  }
+});
+
+app.post('/register', async (req, res) => {
+  user_name = req.body.user;
+  user_email = req.body.email;
+  password = req.body.password;
+
+  const client = await MongoClient.connect(mongo_url);
+  console.log("MongoClient connect");
+  try {
+    const dbo = client.db("tradefolio");
+    var coll = dbo.collection('stocks');
+
+    theQuery = {email:user_email}
+    console.log("theQuery " + user_email);
+
+    var items_found = await coll.findOne(theQuery);
+    if (items_found){
+      console.log("user already exist!");
+      return res.status(400).json({message: "user already exist"});
+    }
+
+    var newData = {"user": user_name, "email": user_email, "password": password};
+    coll.insertOne(newData, function(err, res) {
+      if (err) throw err;
+      console.log("new document inserted");
+    });
+    console.log("Success!");
+    return res.status(200).json({message: "success"});
+
+  } catch (err) {
+      console.log(err);
+  } finally {
+    client.close();
+  }
+})
+
+app.post('/login', async (req, res) => {
+  user_email = req.body.email;
+  password = req.body.password;
+
+  const client = await MongoClient.connect(mongo_url);
+  console.log("MongoClient connect");
+  try {
+    const dbo = client.db("emotion");
+    var coll = dbo.collection('users');
+
+    theQuery = {email:user_email}
+    console.log("theQuery " + user_email);
+
+    var items_found = await coll.findOne(theQuery);
+    if (!items_found){
+      console.log("user not found");
+      return res.status(400).json({message: "user not found"});
+    }
+
+    if(password_hashed != items_found.password){
+      console.log("user found, password wrong");
+      return res.status(400).json({message: "password not match"});
+    }
+    console.log("success  match");
+    return res.status(200).json({message: "success", id: items_found._id});
+  } catch (err) {
+      console.log(err);
+  } finally {
+    client.close();
+  }
 })
